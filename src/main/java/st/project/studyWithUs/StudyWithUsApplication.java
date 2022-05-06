2bb69ae28e45a63a32ad57dcd55e75a9947ebb2a
hyeryun3 <hryun3@gmail.com>
2022-05-06 16:58:19 +0900

    테스트_h

--- src/main/java/st/project/studyWithUs/StudyWithUsApplication.java
+++ src/main/java/st/project/studyWithUs/StudyWithUsApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class StudyWithUsApplication {
-
+// 혜련이
 	public static void main(String[] args) {
 		SpringApplication.run(StudyWithUsApplication.class, args);
 	}
